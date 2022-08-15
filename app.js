56ef3d7b38a12f09bf547850677fa123cd9cda24
ericinkorea <111169637+ericinkorea@users.noreply.github.com>
2022-08-15 23:48:49 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
  const http = require('http'); 
   const os = require('os');
 
-  console.log("Kubia server starting...");
+  console.log("2Kubia server starting...");
 
   var handler = function(request, response) {
       console.log("2Received request from " + request.connection.remoteAddress); 
